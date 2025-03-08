f05a7e3e25325dbe4ec0cf5de12c7ea18db43931
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2025-03-08 17:50:58 +0530

    added new file

---
+++ 05_datatypeSummary.js
@@ -0,0 +1,15 @@
+// Premitive ----> 7 Types
+/* string, number, boolean, null, undefined,symbol, BigInt
+
+Non Premetive (Reference) ----> 3 type
+ Array , Object , Function   */
+
+ const score = 100;
+ const scorevalue = 100.3;
+ const isLoggedId = true;
+ const outSideTemp = null;
+ let userEmailId;
+
+ const id = symbole('123');
+ const anotherId = symbole('123');
+ console.log(id == anotherId);
