aea91c97885676adc1238edcf54c9c3c7f946531
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2025-03-08 12:59:46 +0530

    conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = true
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber )
+console.log( valueInNumber )
+
+//"33" => 33
+//"33ab"=> NaN
+//true => 1; false => 0
+
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+//" " => false
+//"ashu" =>ture
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
