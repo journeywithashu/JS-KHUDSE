ebc150b342b2c499774a9e10f3d352224d9082ae
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2025-03-08 11:28:37 +0530

    Added VARIABLE_AND_CONSTANT files

---
+++ 01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 123321
+let accountEmail = "ashu9519yadav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//acountId = 2 //not allowed
+
+accountEmail = "abs@gmail.com"
+accountPassword = "656565"
+accountCity = "Lucknow"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+Prefer not to use var
+Because of issue in  block scope and functional Scope
+
+
+*/
---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs , not browser
+
+console.log(3+
+    4) //code readibility should be high
+console.log("hitesh") 
+
+
+let name = "ashu"
+let age = 18
+let isloggedIn = false
+let state = null
+let undefined;
+
+
+ //number=> 2 to power 53
+ //bigint
+ //string => " "
+ //boolean => true/false
+ //null => standalone value
+ //undefined =>
+//symbol => unique
+
+ 
+ 
+ //object
+
+ console.log(typeof undefined);//undefined
+ console.log(typeof null); //object
