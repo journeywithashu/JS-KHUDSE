dc3fb4422e31b1fe2c4e0533d5b9172d8211fbad
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2025-03-08 16:05:47 +0530

    "opperations in datatype"

---
+++ 04_comparision.js
@@ -0,0 +1,30 @@
+//console.log(1<2);
+//console.log(1==2);
+//console.log(1<=2);
+//console.log(1>=2);
+//console.log(2!=1);
+
+//console.log("2">1);
+//console.log("02">1);
+
+
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+/*the reason is that an equality check == and
+Comparisions > < => <=works differently
+comparesion convert null to number treating
+ it as 0; */
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// ===
+//console.log("2"===1);
+
+
+
